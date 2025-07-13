a436db1bb9a24c83bf1c83a6f0732ebd22bc8887
Digitalturkk <huseynovfarid1111@gmail.com>
2025-07-13 16:04:51 +0400

    Initial commit

---
+++ TM Backend/src/test/java/TaskManager/TM/TmApplicationTests.java
@@ -0,0 +1,13 @@
+package TaskManager.TM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
